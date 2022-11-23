e4667d228b6fab570021ac1bee119d658b2fa4a7
Valentin Meyer <valentinleomeyer@gmail.com>
2022-11-23 16:45:12 +0100

    added methods to be implemented

---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    private final String playerName;
+
+    public Player (String name) {
+        playerName = name;
+    }
+
+
+}
---
+++ src/Turn.java
@@ -0,0 +1,23 @@
+public class Turn {
+
+    public void playBonusCard (Integer bonus) {
+    }
+
+    public void playCloverLeafCard () {
+    }
+
+    public void playFireWorkCard () {
+    }
+
+    public void playPlusMinusCard () {
+    }
+
+    public void playStopCard () {
+    }
+
+    public void playStraightCard () {
+    }
+
+    public void playX2Card () {
+    }
+}
