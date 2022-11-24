494b4a7e3f6f4310762577de0b157033d15c2020
LexuTros <lennart@toellke.com>
2022-11-24 22:12:30 +0100

    Added basic Methods to Classes and started to implement them

--- src/Helper/Displayer.java
+++ src/Helper/Displayer.java
@@ -1,4 +1,15 @@
 package Helper;
 
 public class Displayer {
+    public static void displayDices() {
+
+    }
+    public static void displayScore() {
+
+    }
+    public static void displayCard() {
+
+    }
+
+
 }
