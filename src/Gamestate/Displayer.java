7c63c4d7c98c245db7559aec44af72d36998143f
LexuTros <lennart@toellke.com>
2022-11-25 20:08:13 +0100

    adjusted Displayer to calls in Round

--- src/Gamestate/Displayer.java
+++ src/Gamestate/Displayer.java
@@ -1,10 +1,14 @@
 package Gamestate;
 
+import Player.Player;
+
+import java.util.List;
+
 public class Displayer {
-    public static void displayDices() {
+    public static void displayDice(List<Integer> numberOfDice) {
 
     }
-    public static void displayScore() {
+    public static void displayScores(List<Player> players) {
 
     }
     public static void displayCard() {
