80fc7e3e7b801c6964035903606e0c2e0e7f9a70
Valentin Meyer <valentinleomeyer@gmail.com>
2022-11-26 10:44:21 +0100

    made DiceRoller static

--- src/Helpers/DiceRoller.java
+++ src/Helpers/DiceRoller.java
@@ -3,32 +3,18 @@ package Helpers;
 import java.util.*;
 public class DiceRoller {
 
+        private static Random ran = new Random();
 
-        private static DiceRoller uniqueInstance = new DiceRoller();
-        private Random ran = new Random();
-        private ArrayList<Integer> rolledDices = new ArrayList<Integer>(Collections.nCopies(6, 0));
-
-
-        public void rollDices(int numberOfDices) {
-            if (!(numberOfDices >=1 && numberOfDices <=6)) {
+        public static ArrayList<Integer> rollDice(int numberOfDice) {
+            ArrayList<Integer> rolledDice = new ArrayList<Integer>(numberOfDice);
+            if (!(numberOfDice >=1 && numberOfDice <=6)) {
                 throw new IllegalArgumentException("Enter a number of dices between 1 and 6!");
             }
-            for (int i = 0; i < numberOfDices; i++) {
-                rolledDices.set(i, ran.nextInt(6) + 1);
+            for (int i = 0; i < numberOfDice; i++) {
+                rolledDice.set(i, ran.nextInt(6) + 1);
             }
-            for(int i = numberOfDices; i < 6; i++) {
-                rolledDices.set(i, 0);
-            }
+            return rolledDice;
         }
-
-        public ArrayList<Integer> getRolledDices() {
-            return rolledDices;
-        }
-
-        public static DiceRoller getInstance() {
-            return uniqueInstance;
-        }
-
     }
 
 
