867188e45125f8db88a00f91376dcc12ea5acba5
wigeto <remo.wiget@gmail.com>
2022-11-27 14:08:08 +0100

    BonusCard with Flyweight. Deck getInstance instead of new.

--- src/Cards/CardBonus.java
+++ src/Cards/CardBonus.java
@@ -1,20 +1,36 @@
 package Cards;
 import Round.Round;
 
+import java.util.ArrayList;
+
 public class CardBonus extends AbstractCard {
 
+    // Flyweight private constructor
+    private CardBonus(int bonus) {
+        this.CardBonus = bonus;
+    }
     // Individual name and bonus of Instance
-    final private String cardName = "Bonus Card";
     private int CardBonus;
-
-    public CardBonus(int bonus) {
-        CardBonus = bonus;
+    final public String cardName = "Bonus Card" + CardBonus;
+    final static private ArrayList<AbstractCard> BonusCards = new ArrayList<>();
+    //Flyweight Factory
+    static {
+        ArrayList<Integer> allPossibleBonus = new ArrayList<Integer>();
+        allPossibleBonus.add(200);
+        allPossibleBonus.add(300);
+        allPossibleBonus.add(400);
+        allPossibleBonus.add(500);
+        allPossibleBonus.add(600);
+        for (int bonus : allPossibleBonus){
+        BonusCards.set(bonus, new CardBonus(bonus));
+        }
     }
 
-    public int getBonus() {
-        return CardBonus;
+    //Flyweight access method
+    public static CardBonus getInstance(Integer bonus) {
+        return (Cards.CardBonus) BonusCards.get(bonus);
     }
-    // abstract method, invoke playRound for this card type
+
     @Override
     public void playRound() {
         Round.playBonusCard(CardBonus);}
