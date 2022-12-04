ef2f6bab443666857dcb4af579df04c8d635879d
Valentin Meyer <valentinleomeyer@gmail.com>
2022-12-04 23:37:22 +0100

    ScoreCalculatorTest -> 100%

--- src/Test/Helpers/TestScoreCalculator.java
+++ src/Test/Helpers/TestScoreCalculator.java
@@ -16,12 +16,14 @@ class TestScoreCalculator {
         final ScoreCalculator sc = new ScoreCalculator();
         List<Integer> someDie = new ArrayList<Integer>(Arrays.asList(1));
         assertEquals(100, sc.calculateScore(someDie));
-        List<Integer> someOtherDices = new ArrayList<Integer>(Arrays.asList(5,5,5,4,4,4));
-        assertEquals(900, sc.calculateScore(someOtherDices));
-        List<Integer> againOtherDices = new ArrayList<Integer>(Arrays.asList(5,5,5,5));
-        assertEquals(550, sc.calculateScore(againOtherDices));
-        List<Integer> againOtherDice = new ArrayList<Integer>(Arrays.asList(5,5,5,5,5,5));
-        assertEquals(1000, sc.calculateScore(againOtherDice));
+        List<Integer> fourFives = new ArrayList<Integer>(Arrays.asList(5,5,5,5));
+        assertEquals(550, sc.calculateScore(fourFives));
+        List<Integer> sixFives = new ArrayList<Integer>(Arrays.asList(5,5,5,5,5,5));
+        assertEquals(1000, sc.calculateScore(sixFives));
+        List<Integer> fiveOnes = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1));
+        assertEquals(1200, sc.calculateScore(fiveOnes));
+        List<Integer> sixOnes = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1,1));
+        assertEquals(2000, sc.calculateScore(sixOnes));
 
 
     }
@@ -32,6 +34,8 @@ class TestScoreCalculator {
         final ScoreCalculator sc = new ScoreCalculator();
         List<Integer> someDices = new ArrayList<Integer>(Arrays.asList(1,1,1,6,6,6));
         assertEquals(1600, sc.calculateScore(someDices));
+        List<Integer> someOtherDices = new ArrayList<Integer>(Arrays.asList(5,5,5,4,4,4));
+        assertEquals(900, sc.calculateScore(someOtherDices));
 
     }
 }
