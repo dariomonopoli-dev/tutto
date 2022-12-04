f201e99004b0613352dfb5a1dd1703b0bf1a8557
wigeto <remo.wiget@gmail.com>
2022-12-05 00:02:35 +0100

    added new tests in Deck and Player

--- src/Test/Cards/TestDeck.java
+++ src/Test/Cards/TestDeck.java
@@ -1,9 +1,7 @@
 package Test.Cards;
 
-import Cards.AbstractCard;
-import org.junit.jupiter.api.Test;
 import Cards.Deck;
-
+import org.junit.jupiter.api.Test;
 
 import java.util.Stack;
 
@@ -12,37 +10,51 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestDeck {
 
     @Test
-    void isEmpty() {
-        final Deck deck = new Deck();
-        assertFalse(deck.isEmpty());
-
-    }
-
-
-    @Test
-    void getTopCard() {
+    void TestGetTopCard() {
         final Deck d = new Deck();
         assertEquals(d.getTopCard().getClass().toString().substring(6,11), d.getTopCard().toString().substring(0,5));
         for (int i = 0; i<56; i++) {
             d.getTopCard();
         }
     }
+
     @Test
-    void getDeckSize() {
+    void TestShuffle(){
+        final Deck deck1 = new Deck();
+        deck1.shuffle();
+        Stack<Object> stack1 = new Stack<>();
+        for(int i =0; i < 10; i++){
+            stack1.push(deck1.getTopCard());
+        }
+
+        Stack<Object> stack2 = new Stack<>();
+        final Deck deck2 = new Deck();
+        for(int i =0; i < 10; i++){
+            stack2.push(deck2.getTopCard());
+        }
+        assertNotEquals(stack1, stack2);
+    }
+
+
+    @Test
+    void TestGetDeckSize() {
         final Deck deck = new Deck();
         deck.initializeDeck();
         assertEquals(56, deck.getDeckSize());
     }
 
     @Test
-    void getInstance() {
+    void TestIsEmpty() {
+        final Deck deck = new Deck();
+        assertFalse(deck.isEmpty());
+    }
+
+    @Test
+    void TestGetInstance() {
         final Deck deck = new Deck();
         deck.initializeDeck();
         final Deck deckInstance = deck.getInstance();
         assertEquals(deckInstance, deck.getInstance());
     }
-
-
-
-    }
+}
 
